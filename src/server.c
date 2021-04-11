b96bdb1d9a12b9cff7c8cc7a92710106e4d5e8c2
Roberto <robertovaquerici@gmail.com>
2021-04-11 16:34:44 +0200

    Get_value done inside worker thread function

--- src/server.c
+++ src/server.c
@@ -15,6 +15,7 @@
 #define MAX_THREADS 10
 #define THREAD_THRESH 2
 #define LISTEN_PORT 23690
+#define KEY_SIZE 255
 
 typedef struct {
     // 1: init      2: set      3: get          4: modify
@@ -22,16 +23,84 @@ typedef struct {
     int32_t op_code;
     char key[255];
     char val1[255];
-    int32_t val2[255];
+    int32_t val2;
     float val3;
-    char new_key[255];
 } Message;
 
 void* worker_thread(void* param) {
     pthread_t this_t = pthread_self();
     pthread_detach(this_t);
     printf("[INFO]  Thread %lu spawned\n", this_t);
+
     int client_socket = *(int*) param;
+    // Receive OP code
+    char c;
+    if(recv(client_socket, &c, sizeof(char), NULL) == -1) {
+        printf("[ERROR][op_code] recv failed with code %s\n", strerror(errno));
+        return 3;
+    }
+    char* key[KEY_SIZE], val1[KEY_SIZE];
+    int32_t val2;
+    float val3;
+    switch (c) {
+        case '1':
+
+            break;
+        case '2':
+
+            break;
+        case '3':
+            // Receive key
+            if(recv(client_socket, &key, sizeof(KEY_SIZE), NULL) == -1) {
+                printf("[ERROR][get_value][key] recv failed with code %s\n", strerror(errno));
+                return 3;
+            }
+            // Receive val1
+            if(recv(client_socket, &val1, sizeof(KEY_SIZE), NULL) == -1) {
+                printf("[ERROR][get_value][val1] recv failed with code %s\n", strerror(errno));
+                return 3;
+            }
+            // Receive val2
+            if(recv(client_socket, &val2, sizeof(KEY_SIZE), NULL) == -1) {
+                printf("[ERROR][get_value][val2] recv failed with code %s\n", strerror(errno));
+                return 3;
+            }
+            // Receive val3
+            if(recv(client_socket, &val3, sizeof(KEY_SIZE), NULL) == -1) {
+                printf("[ERROR][get_value][val3] recv failed with code %s\n", strerror(errno));
+                return 3;
+            }
+            val2 = ntohl(val2);
+            printf("%s, %s, %d, %f\n", key, val1, val2, val3);
+            uint32_t fb_code = 0;
+            int bytes_sent = send(client_socket, fb_code, strlen(fb_code), 0);
+            if(bytes_sent == -1) {
+                printf("[ERROR][get_value][fb_code] send failed with code %s\n", strerror(errno));
+                return -1;
+            }
+            break;
+        case '4':
+
+            break;
+        case '5':
+
+            break;
+        case '6':
+
+            break;
+        case '7':
+
+            break;
+    }
+
+
+
+    char* s = "Server, can you read this?\n";
+    int bytes_sent = send(client_socket, s, strlen(s), 0);
+    if(bytes_sent == -1) {
+        printf("[ERROR] send failed with code %s\n", strerror(errno));
+        return 3;
+    }
     pthread_exit(0);
 }
 
